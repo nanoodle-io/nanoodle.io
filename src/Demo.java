7b0c49127137743ecfbd4bf133db037158417f0e
arranHarty <arranhartgroves@gmail.com>
2018-08-24 09:21:00 +0100

    Renamed Packages, Restructured

---
+++ src/Demo.java
@@ -0,0 +1,9 @@
+import com.nanoodle.NodeQuery;
+
+public class Demo
+{
+	public static void main(String[] args)
+	{
+		NodeQuery.displayHistory("xrb_3ztss8cxo66yoadfg3w7pbynd7q1d8j5xnfakfcejz9ek3wy7pipymwwusfp");
+	}
+}
--- src/com/nanoodle/NodeQuery.java
+++ src/com/nanoodle/NodeQuery.java
@@ -1,16 +1,15 @@
-package com.nanolookup;
+package com.nanoodle;
 
 import java.text.DecimalFormat;
 import java.util.List;
 import java.util.ListIterator;
-
 import org.nano.client.AccountHistory;
 import org.nano.client.History;
 import org.nano.client.NanoClient;
 
-public class AccountLookup
+public class NodeQuery
 {
-	public static void main(String[] args)
+	public static void displayHistory(String accountAddress)
 	{
 		History tempHist;
 		String tempAmount;
@@ -18,16 +17,10 @@ public class AccountLookup
 		String tempDecimal;
 		String tempFormattedAmount;
 		int tempInd;
-		
 		DecimalFormat decimalFormat = new DecimalFormat("0.################################");
 		NanoClient client = new NanoClient();
-		
-		//status
-		client.getBlockCount();
-		client.getNodeVersion();
-		
 		//-1 gets all to open block - we don't know when this was last run
-		AccountHistory tempAcc = client.getAccountHistory(args[0],-1);
+		AccountHistory tempAcc = client.getAccountHistory(accountAddress,-1);
 		List<History> accTransactions = tempAcc.getHistory();
 		
 		//go through and print transactions in list
