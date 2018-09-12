4b571b34010187627bea15145805b0f0366cc4db
arranHarty <arranhartgroves@gmail.com>
2018-09-12 23:55:09 +0100

    refine

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: 'navbar.component.html',
-  styleUrls: ['navbar.component.css']
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
 })
 
 export class NavbarComponent {
