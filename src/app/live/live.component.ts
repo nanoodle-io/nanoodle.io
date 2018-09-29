e576aab9f8b58451254e1f1f33515e43dc736247
arranHarty <arranhartgroves@gmail.com>
2018-09-29 12:29:13 +0100

    Business roadmap and live page query

--- src/app/live/live.component.ts
+++ src/app/live/live.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NodeService } from '../node.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-live-component',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiveComponent implements OnInit {
 
-  constructor() { }
+  error: string;
+  reg = new RegExp('"error"');
+  frontierResults: FrontierResults;
+
+  constructor(private messageService: MessageService, private nodeService: NodeService) { }
 
   ngOnInit() {
+    this.frontierResults = null;
+    this.getFrontierCount();
   }
 
+  getFrontierCount(): void {
+    this.nodeService.getFrontierCount()
+      .subscribe(data => {
+        this.frontierResults = data;
+        if (this.reg.test(JSON.stringify(this.frontierResults))) {
+          this.error = JSON.stringify(this.frontierResults['error']);
+        }
+      });
+  }
+
+  private log(message: string) {
+    this.messageService.add(`Representative Component: ${message}`);
+  }
+}
+
+interface FrontierResults {
+  error?: string;
+  count?: number;
 }
