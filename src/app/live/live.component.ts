8abb0085d2ac842d4edd9fac7fcdf70f636ca9df
arranHarty <arranhartgroves@gmail.com>
2019-01-19 15:57:42 +0000

    price chart and tweaks to legends.

--- src/app/live/live.component.ts
+++ src/app/live/live.component.ts
@@ -10,24 +10,37 @@ import { BaseChartDirective } from 'ng2-charts/ng2-charts';
   styleUrls: ['./live.component.css']
 })
 export class LiveComponent implements OnInit {
-  @ViewChild("baseChart") chart: BaseChartDirective;
-  @ViewChild("baseChart1") chart1: BaseChartDirective;
+  @ViewChild("transactionChart") chart: BaseChartDirective;
+  @ViewChild("currencyChart") chart1: BaseChartDirective;
+  @ViewChild("priceChart") chart2: BaseChartDirective;
 
   blockCountResults: BlockCountResults;
   transactionDatasets = [
-    { data: [1, 2, 3, 4], label: 'Value Transactions' }
+    { data: [1, 1, 1, 1], label: 'Value Transactions' }
   ];
   transactionLabels = [1, 2, 3, 4]
- 
+
   chartOptions = {
     responsive: true
   };
-  
-currencyDatasets = [
-    { data: [1, 2, 3, 4], label: 'Nano' },
-      { data: [1, 2, 3, 4], label: 'GBP' },
-      { data: [1, 2, 3, 4], label: 'USD' },
-      { data: [1, 2, 3, 4], label: 'BTC' }
+
+  currencyDatasets = [
+    { data: [1, 1, 1, 1], label: 'Nano' },
+    { data: [1, 1, 1, 1], label: 'GBP' },
+    { data: [1, 1, 1, 1], label: 'USD' },
+    { data: [1, 1, 1, 1], label: 'CNY' , hidden: true},
+    { data: [1, 1, 1, 1], label: 'JPY' , hidden: true},
+    { data: [1, 1, 1, 1], label: 'EUR' },
+    { data: [1, 1, 1, 1], label: 'BTC' , hidden: true}
+  ];
+
+  priceDatasets = [
+    { data: [1, 1, 1, 1], label: 'GBP' },
+    { data: [1, 1, 1, 1], label: 'USD' },
+    { data: [1, 1, 1, 1], label: 'CNY' , hidden: true},
+    { data: [1, 1, 1, 1], label: 'JPY' , hidden: true},
+    { data: [1, 1, 1, 1], label: 'EUR' },
+    { data: [1, 1, 1, 1], label: 'BTC' , hidden: true}
   ];
 
   constructor(private messageService: MessageService, private NodeService: NodeService, private NetworkService: NetworkService) { }
@@ -47,21 +60,44 @@ currencyDatasets = [
   getChartData(): void {
     let trxTemp = [];
     let timestampTemp = [];
+    //totals
     let nanoTemp = [];
     let gbpTemp = [];
     let usdTemp = [];
     let btcTemp = [];
+    let cnyTemp = [];
+    let jpyTemp = [];
+    let eurTemp = [];
+    //price
+    let gbpPriceTemp = [];
+    let usdPriceTemp = [];
+    let btcPriceTemp = [];
+    let cnyPriceTemp = [];
+    let jpyPriceTemp = [];
+    let eurPriceTemp = [];
+
     this.NetworkService.getChartData(168)
       .subscribe(data => {
         for (var i = 0; i < data.length; i++) {
           trxTemp.push(data[i]['transactions']);
           //work out values
-          nanoTemp.push(this.formatAmount('XRB',+data[i]['rawTotal'],false));
-          gbpTemp.push(+this.formatAmount('XRB',+data[i]['rawTotal'],false)*+data[i]['GBP']);
-          usdTemp.push(+this.formatAmount('XRB',+data[i]['rawTotal'],false)*+data[i]['USD']);
-          btcTemp.push(+this.formatAmount('XRB',+data[i]['rawTotal'],false)*+data[i]['BTC']);
+          nanoTemp.push(this.formatAmount('XRB', +data[i]['rawTotal'], false));
+          gbpTemp.push(+this.formatAmount('XRB', +data[i]['rawTotal'], false) * +data[i]['GBP']);
+          usdTemp.push(+this.formatAmount('XRB', +data[i]['rawTotal'], false) * +data[i]['USD']);
+          cnyTemp.push(+this.formatAmount('XRB', +data[i]['rawTotal'], false) * +data[i]['CNY']);
+          jpyTemp.push(+this.formatAmount('XRB', +data[i]['rawTotal'], false) * +data[i]['JPY']);
+          eurTemp.push(+this.formatAmount('XRB', +data[i]['rawTotal'], false) * +data[i]['EUR']);
+          btcTemp.push(+this.formatAmount('XRB', +data[i]['rawTotal'], false) * +data[i]['BTC']);
+
+          gbpPriceTemp.push(data[i]['GBP']);
+          usdPriceTemp.push(data[i]['USD']);
+          cnyPriceTemp.push(data[i]['CNY']);
+          jpyPriceTemp.push(data[i]['JPY']);
+          eurPriceTemp.push(data[i]['EUR']);
+          btcPriceTemp.push(data[i]['BTC']);
+
           let tempDate = new Date(data[i]['log']['epochTimeStamp']['$date']);
-          timestampTemp.push(this.pad2(tempDate.getDate()) + "-" + this.pad2(tempDate.getMonth() + 1) + "-" + tempDate.getFullYear().toString() + " " +this.pad2(tempDate.getHours()) + ":" + this.pad2(tempDate.getMinutes()));
+          timestampTemp.push(this.pad2(tempDate.getDate()) + "-" + this.pad2(tempDate.getMonth() + 1) + "-" + tempDate.getFullYear().toString() + " " + this.pad2(tempDate.getHours()) + ":" + this.pad2(tempDate.getMinutes()));
         }
         this.transactionLabels = timestampTemp;
         this.transactionDatasets = [
@@ -72,7 +108,19 @@ currencyDatasets = [
           { data: nanoTemp, label: 'Nano' },
           { data: gbpTemp, label: 'GBP' },
           { data: usdTemp, label: 'USD' },
-          { data: btcTemp, label: 'BTC' }
+          { data: cnyTemp, label: 'CNY', hidden: true},
+          { data: jpyTemp, label: 'JPY' , hidden: true},
+          { data: eurTemp, label: 'EUR' },
+          { data: btcTemp, label: 'BTC' , hidden: true}
+        ];
+
+        this.priceDatasets = [
+          { data: gbpPriceTemp, label: 'GBP' },
+          { data: usdPriceTemp, label: 'USD' },
+          { data: cnyPriceTemp, label: 'CNY' , hidden: true},
+          { data: jpyPriceTemp, label: 'JPY' , hidden: true},
+          { data: eurPriceTemp, label: 'EUR' },
+          { data: btcPriceTemp, label: 'BTC' , hidden: true}
         ];
 
         this.reloadChart();
@@ -97,6 +145,15 @@ currencyDatasets = [
       this.chart1.datasets = this.currencyDatasets;
       this.chart1.labels = this.transactionLabels;
       this.chart1.ngOnInit();
+    }
+
+    if (this.chart2 !== undefined) {
+      this.chart2.chart.destroy();
+      this.chart2.chart = 0;
+
+      this.chart2.datasets = this.priceDatasets;
+      this.chart2.labels = this.transactionLabels;
+      this.chart2.ngOnInit();
     }
   }
 
