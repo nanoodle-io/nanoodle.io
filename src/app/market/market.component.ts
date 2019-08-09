9b2020e6b94ce8b9720fa7e24675ffc44bae4522
arranHarty <arranhartgroves@gmail.com>
2019-08-09 18:40:16 +0100

    Add market change component

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MarketService } from '../market.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-market',
@@ -7,19 +8,125 @@ import { MarketService } from '../market.service';
   styleUrls: ['./market.component.css']
 })
 export class MarketComponent implements OnInit {
-lastPrice: number;
-day: number;
-week: number;
-month: number;
+tempRate: number;
 nowDate: Date;
 
-  constructor(private marketService: MarketService) { }
+priceResultsNowUSD: number;
+priceResultsNowBTC: number;
+priceResultsDayUSD: number;
+priceResultsDayBTC: number;
+priceResultsWeekUSD: number;
+priceResultsWeekBTC: number;
+
+  constructor(private marketService: MarketService, private messageService: MessageService) { }
 
   ngOnInit() {
     this.nowDate = new Date();
-    console.log(this.marketService.getMarketPrice(this.nowDate.getTime(), 'USD'));
-    console.log(this.marketService.getMarketPrice(this.nowDate.getTime() - 24 * 60 * 60 * 1000, 'USD'));
-    console.log(this.marketService.getMarketPrice(this.nowDate.getTime() - 24 * 7 * 60 * 60 * 1000, 'USD'));
-    console.log(this.marketService.getMarketPrice(this.nowDate.getTime() - 24 * 7 * 60 * 60 * 1000, 'USD'));
+    this.getPriceNow();
+    this.getPriceDay();
+    this.getPriceWeek();
+  }
+
+  getPriceNow() {
+    this.priceResultsNowUSD = null;
+    this.priceResultsNowBTC = null;
+    this.marketService.getMarketPrice(this.nowDate.getTime(), 'USD')
+      .subscribe(data => {
+        let returnRate = 0;
+        if (data.length > 0) {
+          for (var i = 0; i < data.length; i++) {
+            this.tempRate = data[i];
+            returnRate = returnRate + this.tempRate['USD'];
+          }
+          this.priceResultsNowUSD = returnRate / data.length;
+        }
+        else {
+          this.log("Cannot Retrieve Price Data");
+        }
+      });
+      this.marketService.getMarketPrice(this.nowDate.getTime(), 'BTC')
+      .subscribe(data => {
+        let returnRate = 0;
+        if (data.length > 0) {
+          for (var i = 0; i < data.length; i++) {
+            this.tempRate = data[i];
+            returnRate = returnRate + this.tempRate['BTC'];
+          }
+          this.priceResultsNowBTC = returnRate / data.length;
+        }
+        else {
+          this.log("Cannot Retrieve Price Data");
+        }
+      });
+  }
+
+  getPriceDay() {
+    this.priceResultsDayUSD = null;
+    this.priceResultsDayBTC = null;
+    this.marketService.getMarketPrice(this.nowDate.getTime()-(1*24*60*60*1000), 'USD')
+      .subscribe(data => {
+        let returnRate = 0;
+        if (data.length > 0) {
+          for (var i = 0; i < data.length; i++) {
+            this.tempRate = data[i];
+            returnRate = returnRate + this.tempRate['USD'];
+          }
+          this.priceResultsDayUSD = returnRate / data.length;
+        }
+        else {
+          this.log("Cannot Retrieve Price Data");
+        }
+      });
+      this.marketService.getMarketPrice(this.nowDate.getTime()-(1*24*60*60*1000), 'BTC')
+      .subscribe(data => {
+        let returnRate = 0;
+        if (data.length > 0) {
+          for (var i = 0; i < data.length; i++) {
+            this.tempRate = data[i];
+            returnRate = returnRate + this.tempRate['BTC'];
+          }
+          this.priceResultsDayBTC = returnRate / data.length;
+        }
+        else {
+          this.log("Cannot Retrieve Price Data");
+        }
+      });
+  }
+
+  getPriceWeek() {
+    this.priceResultsWeekUSD = null;
+    this.priceResultsWeekBTC = null;
+    this.marketService.getMarketPrice(this.nowDate.getTime()-(7*24*60*60*1000), 'USD')
+      .subscribe(data => {
+        let returnRate = 0;
+        if (data.length > 0) {
+          for (var i = 0; i < data.length; i++) {
+            this.tempRate = data[i];
+            returnRate = returnRate + this.tempRate['USD'];
+          }
+          this.priceResultsWeekUSD = returnRate / data.length;
+        }
+        else {
+          this.log("Cannot Retrieve Price Data");
+        }
+      });
+      this.marketService.getMarketPrice(this.nowDate.getTime()-(7*24*60*60*1000), 'BTC')
+      .subscribe(data => {
+        let returnRate = 0;
+        if (data.length > 0) {
+          for (var i = 0; i < data.length; i++) {
+            this.tempRate = data[i];
+            returnRate = returnRate + this.tempRate['BTC'];
+          }
+          this.priceResultsWeekBTC = returnRate / data.length;
+        }
+        else {
+          this.log("Cannot Retrieve Price Data");
+        }
+      });
+  }
+
+  private log(message: string) {
+    this.messageService.add(`Account Component: ${message}`);
   }
 }
