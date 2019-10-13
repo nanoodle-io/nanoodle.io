f945171e1b276fd8d5b210e185613acf9f5e6924
arranHarty <arranhartgroves@gmail.com>
2019-10-13 15:49:13 +0100

    format

--- src/scripts/nanoodlelivestats.js
+++ src/scripts/nanoodlelivestats.js
@@ -24,72 +24,65 @@ const dec = 6;
 const raw = 1000000000000000000000000000000;
 
 getConfirmationQuorum().then(data => {
-var  minweight = +(+data['online_stake_total']/1000).toFixed();
-getRepresentatives().then(data => {
-var str = JSON.stringify(data['representatives']);
-var representatives = str.split(",");i
-var prs = "[";
-var prCount = 0;
-var total = 0;
-var totalPercentageDelegated = 0;
-for (var i=0; i<representatives.length; i++)
-{
-var regex = /"(\d+)"/g;
-var regex2 = /(nano_.+)":".*"/g;
-var weight = +regex.exec(representatives[i])[1];
-var rep = regex2.exec(representatives[i])[1];
-if (weight > minweight)
-{
-if (prCount>0)
-{
-prs= prs + ",";
-}
-prCount++;
-prs=prs + "{\"representative\":\"" + rep + "\",\"weight\":" + weight + "}";
-total=total+weight;
-}
-}
-prs = prs + "]";
-var sortable = JSON.parse(prs);
-sortable.sort(function(a, b) {
-    return Number(a.weight) - Number(b.weight);
-});
+    var minweight = +(+data['online_stake_total'] / 1000).toFixed();
+    getRepresentatives().then(data => {
+        var str = JSON.stringify(data['representatives']);
+        var representatives = str.split(","); i
+        var prs = "[";
+        var prCount = 0;
+        var total = 0;
+        var totalPercentageDelegated = 0;
+        for (var i = 0; i < representatives.length; i++) {
+            var regex = /"(\d+)"/g;
+            var regex2 = /(nano_.+)":".*"/g;
+            var weight = +regex.exec(representatives[i])[1];
+            var rep = regex2.exec(representatives[i])[1];
+            if (weight > minweight) {
+                if (prCount > 0) {
+                    prs = prs + ",";
+                }
+                prCount++;
+                prs = prs + "{\"representative\":\"" + rep + "\",\"weight\":" + weight + "}";
+                total = total + weight;
+            }
+        }
+        prs = prs + "]";
+        var sortable = JSON.parse(prs);
+        sortable.sort(function (a, b) {
+            return Number(a.weight) - Number(b.weight);
+        });
 
-for (var i=0; i<sortable.length; i++)
-{
-sortable[i]["percentageDelegated"] = sortable[i]["weight"]/total;
-totalPercentageDelegated = totalPercentageDelegated + sortable[i]["percentageDelegated"];
-sortable[i]["totalPercentageDelegated"] = totalPercentageDelegated;
-}
-var totalAreaUnderLorenz=0;
-for (var i=0; i<sortable.length; i++)
-{
-if (i==0)
-{
-sortable[i]["areaUnderLorenz"]=sortable[i]["totalPercentageDelegated"]/2*(1/prCount);
-}
-else
-{
-sortable[i]["areaUnderLorenz"]=(sortable[i]["totalPercentageDelegated"]+sortable[i-1]["totalPercentageDelegated"])/2*(1/prCount);
-}
-totalAreaUnderLorenz = totalAreaUnderLorenz + sortable[i]["areaUnderLorenz"];
-console.log(sortable[i]);
-}
-var areaOfA = 0.5 - totalAreaUnderLorenz;
-var gini = areaOfA / (areaOfA + totalAreaUnderLorenz);
+        for (var i = 0; i < sortable.length; i++) {
+            sortable[i]["percentageDelegated"] = sortable[i]["weight"] / total;
+            totalPercentageDelegated = totalPercentageDelegated + sortable[i]["percentageDelegated"];
+            sortable[i]["totalPercentageDelegated"] = totalPercentageDelegated;
+        }
+        var totalAreaUnderLorenz = 0;
+        for (var i = 0; i < sortable.length; i++) {
+            if (i == 0) {
+                sortable[i]["areaUnderLorenz"] = sortable[i]["totalPercentageDelegated"] / 2 * (1 / prCount);
+            }
+            else {
+                sortable[i]["areaUnderLorenz"] = (sortable[i]["totalPercentageDelegated"] + sortable[i - 1]["totalPercentageDelegated"]) / 2 * (1 / prCount);
+            }
+            totalAreaUnderLorenz = totalAreaUnderLorenz + sortable[i]["areaUnderLorenz"];
+            console.log(sortable[i]);
+        }
+        var areaOfA = 0.5 - totalAreaUnderLorenz;
+        var gini = areaOfA / (areaOfA + totalAreaUnderLorenz);
 
-console.log("PR Count: " + prCount);
-console.log("Gini: " + gini);
+        console.log("PR Count: " + prCount);
+        console.log("Gini: " + gini);
 
-try {
-                                    console.log("Twitter Alert");
-                                    twitterClient.post('statuses/update', { status: "The $NANO Gini Coefficient currently sits at " + gini.toFixed(4) + " based on the current population of " + prCount + " Principal Representatives (PRs). This is a measure of voting equality, with 0 meaning all PRs have exactly the same voting power delegated to them." });
-                                }
-                                catch (err) {
-                                    console.log(err);
-                                }
+        try {
+            console.log("Twitter Alert");
+            twitterClient.post('statuses/update', { status: "The $NANO Gini Coefficient currently sits at " + gini.toFixed(4) + " based on the current population of " + prCount + " Principal Representatives (PRs). This is a measure of voting equality, with 0 meaning all PRs have exactly the same voting power delegated to them." });
+        }
+        catch (err) {
+            console.log(err);
+        }
 
-});
+    });
 })
     //catch errors from account retrieval
     .catch(error => console.log(error));
